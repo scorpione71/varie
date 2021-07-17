48bf6d5b5f6a556eaa6cce7e4d846d5f8a629d71
Luigi Orlando <luigiorlando71@gmail.com>
2021-07-17 17:35:44 +0200

    ora1

--- varie.go
+++ varie.go
@@ -50,6 +50,5 @@ func main() {
 	prova := user.recuperadati()
 	fmt.Println("Utente : ", prova)
 	fmt.Printf("Utente - Nome : %s\n", user.recuperanome())
-	fmt.Println("Esempio di Interfaccia !!!")
 
 }
